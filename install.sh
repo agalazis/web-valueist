0887fd28688175a723bcaa252d0437109cb07e91
Andreas Galazis <agalazis@gmail.com>
2025-01-11 08:30:59 +0200

    chore(dependencies): add dependencies resolve #1

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo deactivating
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+deactivate
+fi
+echo installing dependencies
+poetry sync
+echo creating symlink
 sudo ln -sfn $PWD/bin/web_valueist /usr/bin/web_valueist
+echo making web_valueist executable
 chmod +x /usr/bin/web_valueist
